2e18bdb4d46f91403c1f8f35c6d9c8dda4195413
P3rlE <97919909+P3rlE@users.noreply.github.com>
2025-08-30 21:25:17 +0200

    Add configurable vehicle health

--- engine/code/game/g_main.c
+++ engine/code/game/g_main.c
@@ -112,6 +112,7 @@ vmCvar_t	g_developer;
 
 vmCvar_t	g_damageScale;
 vmCvar_t	g_vehicleDamageScale;
+vmCvar_t	g_vehicleHealth;
 vmCvar_t  g_humanplayers;
 
 // car variables
@@ -259,7 +260,8 @@ static cvarTable_t		gameCvarTable[] = {
 	{ &car_friction_scale, "car_friction_scale", "1.1", 0, 0, qfalse },
 
 	{ &g_damageScale, "g_damageScale", "0.3", CVAR_ARCHIVE, 0, qfalse },
-	{ &g_vehicleDamageScale, "g_vehicleDamageScale", "0.1", CVAR_ARCHIVE, 0, qfalse },
+	{ &g_vehicleDamageScale, "g_vehicleDamageScale", "1.0", CVAR_ARCHIVE, 0, qfalse },
+	{ &g_vehicleHealth, "g_vehicleHealth", "100", CVAR_ARCHIVE, 0, qfalse },
 // END
 
 	{ &g_rankings, "g_rankings", "0", 0, 0, qfalse},
