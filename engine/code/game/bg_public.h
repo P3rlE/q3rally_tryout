7af04fe2370ed365747af8180d42f4ebdf56e208
P3rlE <97919909+P3rlE@users.noreply.github.com>
2025-08-31 22:56:16 +0200

    Add header guard and conditional includes

--- engine/code/game/bg_public.h
+++ engine/code/game/bg_public.h
@@ -23,6 +23,9 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 // bg_public.h -- definitions shared by both the server game and client game modules
 
+#ifndef BG_PUBLIC_H
+#define BG_PUBLIC_H
+
 // STONELANCE
 #include "bg_physics.h"
 // END
@@ -947,4 +950,6 @@ qboolean        BG_PlayerTouchesItem( playerState_t *ps, entityState_t *item, in
 #define KAMI_SHOCKWAVE_MAXRADIUS                1320
 #define KAMI_BOOMSPHERE_MAXRADIUS               720
 #define KAMI_SHOCKWAVE2_MAXRADIUS               704
+
+#endif // BG_PUBLIC_H
 
