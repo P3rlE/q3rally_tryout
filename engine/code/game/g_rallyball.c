77aeeda3a723ae35360fb10544a15ed1104c5a4c
P3rlE <97919909+P3rlE@users.noreply.github.com>
2025-08-31 13:47:08 +0200

    Add rallyball round and hooks

--- engine/code/game/g_rallyball.c
+++ engine/code/game/g_rallyball.c
@@ -28,6 +28,12 @@ void InitTrigger( gentity_t *self );
 void LogExit( const char *string );
 
 static void G_RallyBall_SpawnBall( void );
+static qboolean G_RallyBall_ScoresTied( void );
+
+static qboolean rbRoundActive;
+static int      rbRoundStartTime;
+static int      rbBallRespawnTime;
+static int      rbLastCountdown;
 
 /*
 =================
@@ -39,7 +45,11 @@ Called when the level is initialized for the rallyball gametype.
 void G_RallyBall_Init( void ) {
     // default spawn origin at world origin; maps can override via rallyball_spawn entity
     VectorClear( level.rallyballSpawn );
-    G_RallyBall_SpawnBall();
+    level.rallyball = NULL;
+    rbRoundActive = qfalse;
+    rbLastCountdown = -1;
+    rbRoundStartTime = level.time + 5000;
+    rbBallRespawnTime = rbRoundStartTime;
 
     if ( g_timelimit.integer ) {
         level.rallyballEndTime = level.time + g_timelimit.integer * 60000;
@@ -63,6 +73,23 @@ static void G_RallyBall_SpawnBall( void ) {
     level.rallyball = G_SpawnRallyBall( level.rallyballSpawn );
 }
 
+static qboolean G_RallyBall_ScoresTied( void ) {
+    int i;
+    int top = -1;
+    int second = -1;
+
+    for ( i = TEAM_RED; i <= TEAM_YELLOW; i++ ) {
+        if ( level.teamScores[i] > top ) {
+            second = top;
+            top = level.teamScores[i];
+        } else if ( level.teamScores[i] > second ) {
+            second = level.teamScores[i];
+        }
+    }
+
+    return top == second;
+}
+
 /*
 =================
 G_RallyBall_RunFrame
@@ -71,8 +98,30 @@ Per-frame updates for rallyball.
 =================
 */
 void G_RallyBall_RunFrame( void ) {
+    if ( !rbRoundActive ) {
+        int secs = ( rbRoundStartTime - level.time + 999 ) / 1000;
+        if ( secs > 0 ) {
+            if ( secs != rbLastCountdown ) {
+                trap_SendServerCommand( -1, va( "cp \"%i\"", secs ) );
+                rbLastCountdown = secs;
+            }
+            return;
+        }
+        rbRoundActive = qtrue;
+        trap_SendServerCommand( -1, "cp \"GO!\"" );
+    }
+
+    if ( !level.rallyball && level.time >= rbBallRespawnTime ) {
+        G_RallyBall_SpawnBall();
+    }
+
     if ( level.rallyballEndTime && level.time >= level.rallyballEndTime ) {
-        LogExit( "Rallyball time limit hit." );
+        if ( G_RallyBall_ScoresTied() ) {
+            level.rallyballEndTime = level.time + 60000;
+            trap_SendServerCommand( -1, "cp \"Overtime!\"" );
+        } else {
+            LogExit( "Rallyball time limit hit." );
+        }
     }
 }
 
@@ -123,7 +172,8 @@ static void G_RallyBall_GoalTouch( gentity_t *self, gentity_t *other, trace_t *t
         level.rallyball = NULL;
     }
     G_FreeEntity( other );
-    G_RallyBall_SpawnBall();
+    rbBallRespawnTime = level.time + 3000;
+    CalculateRanks();
 }
 
 /*QUAKED trigger_rallyball_goal (.5 .5 .5) ? RED_GOAL BLUE_GOAL GREEN_GOAL YELLOW_GOAL
