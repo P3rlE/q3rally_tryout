060322488c410ee7c4ba10a0711461cbcf1df967
P3rlE <97919909+P3rlE@users.noreply.github.com>
2025-09-03 22:40:19 +0200

    Add trigger_fuel spawn prototype

--- engine/code/game/g_spawn.c
+++ engine/code/game/g_spawn.c
@@ -148,6 +148,7 @@ void SP_trigger_multiple (gentity_t *ent);
 void SP_trigger_push (gentity_t *ent);
 void SP_trigger_teleport (gentity_t *ent);
 void SP_trigger_hurt (gentity_t *ent);
+void SP_trigger_fuel (gentity_t *ent);
 
 void SP_target_remove_powerups( gentity_t *ent );
 void SP_target_give (gentity_t *ent);
