16d9d5e8ba874be834ddc7a1faa27f8d59084746
perle <per-thormann@gmx.de>
2025-09-07 17:40:52 +0200

    fix

--- engine/code/cgame/cg_rally_hud2.c
+++ engine/code/cgame/cg_rally_hud2.c
@@ -32,6 +32,7 @@ void CG_DrawHUD_Times(float x, float y){
 	centity_t	*cent;
 	int			lapTime, lastTime, totalTime, teamTime;
 	//int		bestTime;
+	char		*time;
 	int			i, count = 0;
 
 	cent = &cg_entities[cg.snap->ps.clientNum];
@@ -352,6 +353,8 @@ void CG_DrawHUD_DerbyList(float x, float y){
 	int			i;
 	vec4_t		color;
 	centity_t	*cent;
+	char		*time;
+	float		playTime;
 
 	// draw heading
     x = 636 - 120;
@@ -360,6 +363,8 @@ void CG_DrawHUD_DerbyList(float x, float y){
 	// name
 	CG_DrawTinyDigitalStringColor( x + 16, y, "P:", colorWhite);
 
+	// time
+//	CG_DrawTinyStringColor( x + 70, y, "TIME:", colorWhite);
 
 	// dmg dealt
 	CG_DrawTinyDigitalStringColor( x + 70, y, "DD:", colorWhite);
@@ -387,6 +392,14 @@ void CG_DrawHUD_DerbyList(float x, float y){
 			Vector4Copy(colorMdGrey, color);
 		}
 
+		playTime = 0;
+		if (cent->finishRaceTime){
+			playTime = cent->finishRaceTime - cent->startLapTime;
+		}
+		else if (cent->startRaceTime){
+			playTime = cg.time - cent->startLapTime;
+		}
+		time = getStringForTime(playTime);
 
 		// num
 		CG_DrawTinyDigitalStringColor( x + 2, y, va("%i", (i+1)), color);
@@ -394,6 +407,8 @@ void CG_DrawHUD_DerbyList(float x, float y){
 		// name
 		CG_DrawTinyDigitalStringColor( x + 16, y, cgs.clientinfo[cg.scores[i].client].name, color);
 
+		// time
+//		CG_DrawTinyStringColor( x + 70, y, time, color);
 
 		// dmg dealt
 		CG_DrawTinyDigitalStringColor( x + 75, y, va("%i", cg.scores[i].damageDealt), color);
