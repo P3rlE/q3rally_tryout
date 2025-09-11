912ff8e6f6d8a87c5bc9ba3be7a4a7b8cf9b276e
perle <per-thormann@gmx.de>
2025-09-11 06:13:22 +0200

    .

--- engine/code/cgame/cg_main.c
+++ engine/code/cgame/cg_main.c
@@ -24,7 +24,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // cg_main.c -- initialization and primary entry point for cgame
 #include "cg_local.h"
 #include "../client/keycodes.h"
-#include "string.h"
+#include <string.h>
 
 #ifdef MISSIONPACK
 #include "../ui/ui_shared.h"
