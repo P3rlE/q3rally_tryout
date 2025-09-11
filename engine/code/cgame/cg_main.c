387abd7f09b07afb3552e244e95a1511f43014a9
perle <per-thormann@gmx.de>
2025-09-11 21:15:51 +0200

    ...

--- engine/code/cgame/cg_main.c
+++ engine/code/cgame/cg_main.c
@@ -1,4 +1,4 @@
-/*
+d/*
 ===========================================================================
 Copyright (C) 1999-2005 Id Software, Inc.
 Copyright (C) 2002-2025 Q3Rally Team (Per Thormann - perle@q3rally.com)
