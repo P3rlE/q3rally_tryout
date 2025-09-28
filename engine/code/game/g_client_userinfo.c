700565caa43a3eab7623511050dad38321658108
P3rlE <97919909+P3rlE@users.noreply.github.com>
2025-09-28 19:23:23 +0200

    Guard g_client_userinfo string header for VM builds

--- engine/code/game/g_client_userinfo.c
+++ engine/code/game/g_client_userinfo.c
@@ -1,7 +1,11 @@
 #include "g_local.h"
 #include "g_client_userinfo.h"
 
+#ifdef Q3_VM
+#include "bg_lib.h"
+#else
 #include <string.h>
+#endif
 
 qboolean G_SetClientConfigstringIfChanged( int clientNum, const char *playerConfigString ) {
     char currentConfigString[MAX_INFO_STRING];
