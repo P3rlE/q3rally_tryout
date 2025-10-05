40de91c8a6857683aaed3d89d855e00cee962184
P3rlE <97919909+P3rlE@users.noreply.github.com>
2025-10-05 20:14:31 +0200

    Include q_shared.h for plate utility header

--- engine/code/qcommon/q_shared_plates.h
+++ engine/code/qcommon/q_shared_plates.h
@@ -1,6 +1,8 @@
 #ifndef Q_SHARED_PLATES_H
 #define Q_SHARED_PLATES_H
 
+#include "q_shared.h"
+
 qboolean Q3R_CreateLicensePlateImage( const char *templateImage,
                                       const char *outputImage,
                                       const char *name,
