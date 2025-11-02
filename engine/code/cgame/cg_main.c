5dfd44236976b4b7ea02e281d18d6a64c6ea9bec
P3rlE <97919909+P3rlE@users.noreply.github.com>
2025-11-02 21:18:03 +0100

    Add team sound events for all CTF4 teams

--- engine/code/cgame/cg_main.c
+++ engine/code/cgame/cg_main.c
@@ -763,6 +763,8 @@ static void CG_RegisterSounds( void ) {
 		if ( cgs.gametype == GT_CTF || cgs.gametype == GT_DOMINATION || cg_buildScript.integer ) {
 			cgs.media.redFlagReturnedSound = trap_S_RegisterSound( "sound/teamplay/voc_red_returned.wav", qtrue );
 			cgs.media.blueFlagReturnedSound = trap_S_RegisterSound( "sound/teamplay/voc_blue_returned.wav", qtrue );
+			cgs.media.greenFlagReturnedSound = trap_S_RegisterSound( "sound/teamplay/voc_green_returned.wav", qtrue );
+			cgs.media.yellowFlagReturnedSound = trap_S_RegisterSound( "sound/teamplay/voc_yellow_returned.wav", qtrue );
 			cgs.media.enemyTookYourFlagSound = trap_S_RegisterSound( "sound/teamplay/voc_enemy_flag.wav", qtrue );
 			cgs.media.yourTeamTookEnemyFlagSound = trap_S_RegisterSound( "sound/teamplay/voc_team_flag.wav", qtrue );
 		}
