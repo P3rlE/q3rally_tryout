351406f2940c2fd8c0c22b1e051d9dfdc8994905
P3rlE <q3rally@gmail.com>
2026-02-21 02:23:12 +0100

    fix

--- engine/code/cgame/cg_local.h
+++ engine/code/cgame/cg_local.h
@@ -1602,11 +1602,9 @@ extern	vmCvar_t		cg_jukeboxRepeatMode;
 extern	vmCvar_t		cg_derbyHitFxEnable;
 extern	vmCvar_t		cg_derbyHitShakeScale;
 extern	vmCvar_t		cg_derbyHitSoundEnable;
-<<<<<<< codex/add-tiered-impact-system-for-derby-hits-x1lkw3
 extern	vmCvar_t		cg_derbyHitOverlayScale;
 extern	vmCvar_t		cg_derbyHitOverlayTime;
-=======
->>>>>>> master
+
 // Q3Rally Code END
 
 //
