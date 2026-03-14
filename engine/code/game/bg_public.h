d9ad14981b6cb346e3c270bd2b96a0099a3433c7
P3rlE <97919909+P3rlE@users.noreply.github.com>
2026-03-14 22:54:30 +0100

    Register KOTH sigil shader assets for hill indicator

--- engine/code/game/bg_public.h
+++ engine/code/game/bg_public.h
@@ -108,7 +108,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define CS_REFLECTION_IMAGE             29
 #define CS_SIGILSTATUS                  30
 #define CS_TRACKLENGTH                  31
-#define CS_KOTHSTATUS                   32  // "owner capture_pct" for KOTH hill state
+#define CS_KOTHSTATUS                   32  // "owner contested capture_pct [x y z]" for KOTH hill state
 // Q3Rally Code END
 
 #define CS_MODELS                               33
