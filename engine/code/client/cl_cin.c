7d6fdb63aef300cd41b81d7e8230e8a6f2fe2456
P3rlE <q3rally@gmail.com>
2026-03-18 17:01:48 +0100

    fix

--- engine/code/client/cl_cin.c
+++ engine/code/client/cl_cin.c
@@ -1205,8 +1205,10 @@ redump:
 			break;
 		case	ZA_SOUND_STEREO:
 			if (!cinTable[currentHandle].silent) {
-				if (cinTable[currentHandle].numQuads == -1) {
-					S_Update();
+				if (cinTable[currentHandle].numQuads == -1 || s_rawend[0] < s_soundtime) {
+					// Don't call S_Update() here: it advances s_soundtime and causes
+					// s_rawend to fall behind, making RawSamples discard the first
+					// audio chunks (no-sound bug). Sync rawend directly instead.
 					s_rawend[0] = s_soundtime;
 				}
 				ssize = RllDecodeStereoToStereo( framedata, sbuf, cinTable[currentHandle].RoQFrameSize, 0, (unsigned short)cinTable[currentHandle].roq_flags);
@@ -1498,7 +1500,7 @@ if (cinTable[currentHandle].fileType == FT_OGM)
 	if (cinTable[currentHandle].shader && (abs(thisTime - cinTable[currentHandle].lastTime))>100) {
 		cinTable[currentHandle].startTime += thisTime - cinTable[currentHandle].lastTime;
 	}
-	cinTable[currentHandle].tfps = (((CL_ScaledMilliseconds() - cinTable[currentHandle].startTime)*3)/100);
+	cinTable[currentHandle].tfps = (((CL_ScaledMilliseconds() - cinTable[currentHandle].startTime) * cinTable[currentHandle].roqFPS) / 1000);
 
 	start = cinTable[currentHandle].startTime;
 	while(  (cinTable[currentHandle].tfps != cinTable[currentHandle].numQuads)
@@ -1506,7 +1508,7 @@ if (cinTable[currentHandle].fileType == FT_OGM)
 	{
 		RoQInterrupt();
 		if (start != cinTable[currentHandle].startTime) {
-			cinTable[currentHandle].tfps = (((CL_ScaledMilliseconds() - cinTable[currentHandle].startTime)*3)/100);
+			cinTable[currentHandle].tfps = (((CL_ScaledMilliseconds() - cinTable[currentHandle].startTime) * cinTable[currentHandle].roqFPS) / 1000);
 			start = cinTable[currentHandle].startTime;
 		}
 	}
