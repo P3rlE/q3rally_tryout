ca8469fbadd8422dc64f13245b6802e141a94412
P3rlE <97919909+P3rlE@users.noreply.github.com>
2026-03-21 23:00:56 +0100

    Improve local engine audio control signals

---
+++ engine/code/qcommon/engine_audio_shared.h
@@ -0,0 +1,34 @@
+#ifndef ENGINE_AUDIO_SHARED_H
+#define ENGINE_AUDIO_SHARED_H
+
+#include "q_shared.h"
+
+typedef enum engineAudioQualityTier_e {
+    EA_QUALITY_OFF = 0,
+    EA_QUALITY_FAR,
+    EA_QUALITY_NEAR,
+    EA_QUALITY_HERO
+} engineAudioQualityTier_t;
+
+typedef struct vehicleAudioState_s {
+    float rpm;
+    float rpmNorm;
+
+    float throttle;
+    float load;
+    float speed;
+
+    float clutchSlip;
+    float wheelSlip;
+    float turboBoost;
+
+    int gear;
+
+    qboolean ignitionCut;
+    qboolean fuelCut;
+    qboolean limiterActive;
+    qboolean backfireEvent;
+    qboolean damaged;
+} vehicleAudioState_t;
+
+#endif /* ENGINE_AUDIO_SHARED_H */
