ef8dc1b8e9c58dee5bfea1945b03c175f85bc8bb
P3rlE <97919909+P3rlE@users.noreply.github.com>
2026-03-22 23:33:49 +0100

    Tame high-rpm engine audio brightness

---
+++ engine/code/client/snd_engine_dsp.h
@@ -0,0 +1,58 @@
+#ifndef SND_ENGINE_DSP_H
+#define SND_ENGINE_DSP_H
+
+#include "snd_engine_audio.h"
+
+typedef struct engineAudioBiquadState_s {
+    float x1, x2;
+    float y1, y2;
+} engineAudioBiquadState_t;
+
+typedef struct engineAudioResonatorState_s {
+    engineAudioBiquadState_t biquad;
+} engineAudioResonatorState_t;
+
+typedef struct engineAudioSynthState_s {
+    float sampleRate;
+
+    float phase;
+    float crankPhase;
+    float combustionPhase;
+    float combustionJitter;
+    float combustionTone;
+    float combustionNoise;
+    int firingOrderIndex;
+
+    float smoothedRpm;
+    float smoothedThrottle;
+    float smoothedLoad;
+
+    float limiterEnvelope;
+    float backfireEnvelope;
+    float overrunPopEnvelope;
+
+    float cockpitLowpassL;
+    float cockpitLowpassR;
+    float toneLowpassL;
+    float toneLowpassR;
+
+    unsigned int noiseSeed;
+
+    engineAudioResonatorState_t exhaustStates[MAX_ENGINE_AUDIO_RESONATORS];
+    engineAudioResonatorState_t intakeStates[MAX_ENGINE_AUDIO_RESONATORS];
+
+    float harmonicPhase[MAX_ENGINE_AUDIO_HARMONICS];
+} engineAudioSynthState_t;
+
+void S_EngineDSP_Reset( engineAudioSynthState_t *state, float sampleRate );
+void S_EngineDSP_RenderEmitter(
+    engineAudioSynthState_t *synth,
+    const engineAudioPreset_t *preset,
+    const vehicleAudioState_t *control,
+    engineAudioQualityTier_t quality,
+    int sampleCount,
+    float *outLeft,
+    float *outRight );
+void S_EngineDSP_TriggerBackfire( engineAudioSynthState_t *state );
+
+#endif /* SND_ENGINE_DSP_H */
